90d22c4b123624cd1e27b08b6d1ce060de68dfbc
Vishnu <153791406+vardhan998997@users.noreply.github.com>
2025-06-11 09:41:09 +0530

    Time: 10 ms (52.50%) | Memory: 79.7 MB (34.00%) - LeetSync

--- 3373-maximum-prime-difference/maximum-prime-difference.java
+++ 3373-maximum-prime-difference/maximum-prime-difference.java
@@ -6,12 +6,9 @@ class Solution {
         isprime[1] = true;
         for(int  i = 2;i<=Math.sqrt(max);i++){
             for(int j = i*i;j<=max;j+=i){
-                isprime[j] = true;
+               if(!isprime[j]) isprime[j] = true;
             }
         }
-
-        System.out.print(Arrays.toString(isprime));
-
 
         int min_prime = -1 , max_prime = -1;
         boolean isflag = true;
@@ -21,12 +18,10 @@ class Solution {
                     min_prime = i;
                     isflag = false;
                 }
-
                 max_prime = i;
             }
         }
-        System.out.print(min_prime);
-        System.out.print(max_prime);
+
         if(min_prime==-1 || max_prime==-1){
             return 0;
         }else{
