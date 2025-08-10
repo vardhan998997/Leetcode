37e4153ac6771ec5e45c7c522af83fbc1073c426
Vishnu <153791406+vardhan998997@users.noreply.github.com>
2025-08-10 18:36:09 +0530

    Time: 52 ms (39.68%) | Memory: 168.2 MB (31.26%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.java
@@ -0,0 +1,48 @@
+class Node{
+    int first;
+    int second;
+    int val;
+    Node(int first, int second, int val){
+        this.first = first;
+        this.second = second;
+        this.val = val;
+    }
+}
+
+class Solution {
+    public int[][] highestPeak(int[][] isWater) {
+        int n = isWater.length, m = isWater[0].length;
+        int[][] ans = new int[n][m];
+        Queue<Node> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[n][m];
+
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(isWater[i][j]==0){
+                    ans[i][j] = 1;
+                }else{
+                    queue.offer(new Node(i,j,0));
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
+        while(!queue.isEmpty()){
+            Node newNode = queue.poll();
+            int row = newNode.first, col = newNode.second, val = newNode.val;
+            ans[row][col] = val;
+
+            for(int[] di : dir){
+                int newRow = row + di[0];
+                int newCol = col + di[1];
+
+                if(newRow>=0 && newCol>=0 && newRow<n && newCol<m && !visited[newRow][newCol]){
+                    visited[newRow][newCol] = true;
+                    queue.offer(new Node(newRow, newCol, val+1));
+                }
+            }
+        }
+        return ans;
+    }
+}
