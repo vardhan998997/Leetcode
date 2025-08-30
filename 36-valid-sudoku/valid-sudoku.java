70ee3996310221ff95a25837e9d41d513a1a8d8b
Vishnu <153791406+vardhan998997@users.noreply.github.com>
2025-08-30 11:27:01 +0530

    Time: 3 ms (52.39%) | Memory: 44.3 MB (78.74%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -5,34 +5,33 @@ class Solution {
         HashSet<Character>[] col = new HashSet[n];
         HashSet<Character>[] boxes = new HashSet[n];
 
-        for(int i = 0;i<n;i++){
-            row[i] = new HashSet<Character>();
-            col[i] = new HashSet<Character>();
-            boxes[i] = new HashSet<Character>();
-        }
+    for(int i = 0;i<n;i++){
+        row[i] = new HashSet<Character>();
+        col[i] = new HashSet<Character>();
+        boxes[i] = new HashSet<Character>();
+    }
 
-        for(int r = 0;r<row.length;r++){
-            for(int c = 0;c<board[0].length;c++){
-                char cell = board[r][c];
-                if(cell=='.'){
-                    continue;
-                }
-                if(row[r].contains(cell)){
-                    return false;
-                }
-                row[r].add(cell);
-                if(col[c].contains(cell)){
-                    return false;
-                }
-                col[c].add(cell);
-                int index = 3 * (r/3) + (c/3);
-                if(boxes[index].contains(cell)){
-                    return false;
-                }
-                boxes[index].add(cell);
+    for(int r = 0;r<board.length;r++){
+        for(int c = 0;c<board[0].length;c++){
+            char cell = board[r][c];
+            if(cell=='.'){
+                continue;
             }
+            if(row[r].contains(cell)){
+                return false;
+            }
+            row[r].add(cell);
+            if(col[c].contains(cell)){
+                return false;
+            }
+            col[c].add(cell);
+            int idx = 3 * (r/3) + (c/3);
+            if(boxes[idx].contains(cell)){
+                return false;
+            }
+            boxes[idx].add(cell);
         }
+    }
         return true;
     }
 }
-
