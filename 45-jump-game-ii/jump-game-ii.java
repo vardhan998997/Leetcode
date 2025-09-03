0d1f28fddb6616527fe8e8e78a2db0feacac0779
Vishnu <153791406+vardhan998997@users.noreply.github.com>
2025-09-03 12:15:40 +0530

    Time: 1 ms (99.56%) | Memory: 45.1 MB (73.38%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,21 +1,16 @@
 class Solution {
     public int jump(int[] nums) {
         int r = 0, l = 0, foremost = 0, steps = 0, n = nums.length;
-        if(n==1) return 0;
 
-        while(r<n){
+        while(r<n-1){
+
             for(int i = l;i<=r;i++){
                 foremost = Math.max(foremost, nums[i]+i);
             }
 
             l = r + 1;
             r = foremost;
-
             steps++;
-
-            if(foremost>=n-1){
-                break;
-            }
         }
         return steps;
     }
