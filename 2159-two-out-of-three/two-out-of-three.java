b813e965e231ab7ea0e3da6717ff5e2b3ecad516
Vishnu <153791406+vardhan998997@users.noreply.github.com>
2025-09-27 18:03:10 +0530

    Time: 1 ms (100.00%) | Memory: 44.8 MB (72.02%) - LeetSync

--- 2159-two-out-of-three/two-out-of-three.java
+++ 2159-two-out-of-three/two-out-of-three.java
@@ -1,25 +1,25 @@
 class Solution {
-    public List<Integer> twoOutOfThree(int[] arr1, int[] arr2, int[] arr3) {
-        int nums1[] = Arrays.stream(arr1).distinct().toArray();
-        int nums2[] = Arrays.stream(arr2).distinct().toArray();
-        int nums3[] = Arrays.stream(arr3).distinct().toArray();
+    public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
 
-        int[] freq = new int[101];
+        int[] freq1 = new int[101];
+        int[] freq2 = new int[101];
+        int[] freq3 = new int[101];
+
         for(int i = 0;i<nums1.length;i++){
-                freq[nums1[i]]++;
+                freq1[nums1[i]]++;
         }
 
         for(int i = 0;i<nums2.length;i++){
-                freq[nums2[i]]++;
+                freq2[nums2[i]]++;
         }
 
         for(int i = 0;i<nums3.length;i++){
-                freq[nums3[i]]++;
+                freq3[nums3[i]]++;
         }
 
         ArrayList<Integer> ans = new ArrayList<>();
         for(int i = 1;i<=100;i++){
-            if(freq[i]>1){
+            if(freq1[i]>0 && freq2[i]>0 || freq2[i]>0 && freq3[i]>0 || freq3[i]>0 && freq1[i]>0){
                 ans.add(i);
             }
         }
