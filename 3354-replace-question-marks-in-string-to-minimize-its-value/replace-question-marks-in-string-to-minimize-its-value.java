4f264bccfe882e26f4819496b18f0f7df8fc8fbc
Vishnu <153791406+vardhan998997@users.noreply.github.com>
2025-09-30 10:47:07 +0530

    Time: 38 ms (86.52%) | Memory: 45.8 MB (91.01%) - LeetSync

--- 3354-replace-question-marks-in-string-to-minimize-its-value/replace-question-marks-in-string-to-minimize-its-value.java
+++ 3354-replace-question-marks-in-string-to-minimize-its-value/replace-question-marks-in-string-to-minimize-its-value.java
@@ -5,20 +5,19 @@ class Solution {
         for(char ch : s.toCharArray()){
             if(ch!='?') freq[ch-'a']++;
         }
-        String replaced = "";
+        int replaced[] = new int[26];
         for(int i = 0;i<s.length();i++){
             if(s.charAt(i)=='?'){
                 int idx = findMin(freq);
                 freq[idx]++;
-                replaced += (char)(idx + 'a') + "";
+                replaced[idx]++;
             }
         }
-        char charc[] = replaced.toCharArray();
-        Arrays.sort(charc);
+        
         int idx = 0;
         for(int i = 0;i<s.length();i++){
             if(s.charAt(i)=='?'){
-                sb.setCharAt(i,charc[idx++]);
+                sb.setCharAt(i,nextIdx(replaced));
             }
         }
 
@@ -36,5 +35,15 @@ class Solution {
             if(min == 0) return idx;
         }
         return idx;
+    }
+
+    public char nextIdx(int replaced[]){
+        for(int i = 0;i<26;i++){
+            if(replaced[i]>0){
+                replaced[i]--;
+                return (char)(i+'a');
+            }
+        }
+        return 'a';
     }
 }
