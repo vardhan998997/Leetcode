d2ba0832db52f55ad9228225c4ba2e3adefc74ac
Vishnu <153791406+vardhan998997@users.noreply.github.com>
2025-10-01 16:23:26 +0530

    Time: 3 ms (30.32%) | Memory: 44.8 MB (41.83%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,31 +1,35 @@
 class Solution {
-    int tempCount = 0;
     public int maxAreaOfIsland(int[][] grid) {
         int n = grid.length, m = grid[0].length;
         int ans = 0;
 
+        int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
+
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
                 if(grid[i][j]==1){
-                    tempCount = 0;
-                    dfs(i, j, n, m, grid);
-                    ans = Math.max(ans, tempCount);
+                    int temp = 0;
+                    Queue<int[]> queue = new LinkedList<>();
+                    queue.offer(new int[]{i,j});
+                    grid[i][j] = 0;
+
+                    while(!queue.isEmpty()){
+                        int[] cell = queue.poll();
+                        temp++;
+
+                        for(int[] di : dir){
+                            int newRow = di[0] + cell[0];
+                            int newCol = di[1] + cell[1];
+                            if(newRow>=0 && newCol>=0 && newRow<n && newCol<m && grid[newRow][newCol]==1){
+                                queue.offer(new int[]{newRow, newCol});
+                                grid[newRow][newCol] = 0;
+                            }
+                        }
+                    }
+                    ans = Math.max(ans, temp);
                 }
             }
         }
         return ans;
-    }
-
-    public void dfs(int row, int col, int n, int m, int[][] grid){
-        if(row<0 || col<0 || row>=n || col>=m || grid[row][col]==0){
-            return;
-        }
-        tempCount++;
-        grid[row][col] = 0;
-
-        dfs(row-1, col, n, m, grid);
-        dfs(row+1, col, n, m, grid);
-        dfs(row, col-1, n, m, grid);
-        dfs(row, col+1, n, m, grid);
     }
 }
