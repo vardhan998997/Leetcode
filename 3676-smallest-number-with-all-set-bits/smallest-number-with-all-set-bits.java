5bddce4fabb465558f4671d87c868dc378ee08ba
Vishnu <153791406+vardhan998997@users.noreply.github.com>
2025-10-30 09:48:31 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (26.28%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.java
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.java
@@ -1,11 +1,14 @@
 class Solution {
     public int smallestNumber(int n) {
-        for(int i = 0;i<n;i++){
-            int power = (int)Math.pow(2, i);
-            if(power>n){
-                return power-1;
-            }
-        }
-        return 1;
+        // for(int i = 0;i<n;i++){
+        //     int power = (int)Math.pow(2, i);
+        //     if(power>n){
+        //         return power-1;
+        //     }
+        // }
+        // return 1;
+
+        int p = (int)(Math.log(n) / Math.log(2));
+        return (int)Math.pow(2, p+1) - 1;
     }
 }
