53e1a40de36cca3dafc3e231ea90f1934442eff6
Vishnu <153791406+vardhan998997@users.noreply.github.com>
2025-11-01 13:18:07 +0530

    Time: 6 ms (8.02%) | Memory: 47.7 MB (5.61%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -3,16 +3,35 @@ class Solution {
         int n = board.length, m = board[0].length;
         if(n==0 || n==1 || m == 1 || m == 0) return;
 
+        Queue<int[]> queue = new LinkedList<>();
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
                 if(i==0 || j==0 || i==n-1 || j==m-1){
                     if(board[i][j]=='O'){
-                        dfs(i, j, n, m, board);
+                        queue.offer(new int[]{i,j});
                     }
                 }
             }
         }
 
+        int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
+        /// bfs
+        while(!queue.isEmpty()){
+            int[] cell = queue.poll();
+            board[cell[0]][cell[1]] = '!';
+            for(int[] di : dir){
+                int newRow = cell[0] + di[0];
+                int newCol = cell[1] + di[1];
+
+                if(newRow>=0 && newCol>=0 && newRow<n && newCol<m && board[newRow][newCol]=='O'){
+                    board[newRow][newCol] = '!';
+                    queue.offer(new int[]{newRow, newCol});
+                }
+            }
+        }
+
+
+        /////// 
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
                 if(board[i][j] == 'O'){
@@ -23,17 +42,5 @@ class Solution {
                 }
             }
         }
-    }
-
-    public void dfs(int row, int col, int n, int m, char[][] board){
-        if(row<0 || col<0 || row>=n || col>=m || board[row][col]=='X' || board[row][col]=='!'){
-            return;
-        }
-
-        board[row][col] = '!';
-        dfs(row-1, col, n, m, board);
-        dfs(row+1, col, n, m, board);
-        dfs(row, col-1, n, m, board);
-        dfs(row, col+1, n, m, board);
     }
 }
