a5ee7c4431fe330d7707e4845426dd223b82e3d7
Vishnu <153791406+vardhan998997@users.noreply.github.com>
2025-11-01 11:34:50 +0530

    Time: 2 ms (83.28%) | Memory: 48 MB (5.38%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,14 +1,13 @@
 class Solution {
     public void solve(char[][] board) {
-        int n = board.length , m = board[0].length;
-        if(n==0 || n==1 || m==1) return;
-        boolean visited[][] = new boolean[n][m];
+        int n = board.length, m = board[0].length;
+        if(n==0 || n==1 || m == 1 || m == 0) return;
 
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
                 if(i==0 || j==0 || i==n-1 || j==m-1){
-                    if(board[i][j]=='O' && !visited[i][j]){
-                        dfs(i,j,n,m, board, visited);
+                    if(board[i][j]=='O'){
+                        dfs(i, j, n, m, board);
                     }
                 }
             }
@@ -16,23 +15,25 @@ class Solution {
 
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
-                if(!visited[i][j]){
+                if(board[i][j] == 'O'){
                     board[i][j] = 'X';
+                } 
+                if(board[i][j]=='!'){
+                    board[i][j] = 'O';
                 }
             }
         }
     }
 
-    public void dfs(int row, int col, int n, int m, char[][] board, boolean[][] visited){
-        if(row<0 || col<0 || row>=n || col>=m || visited[row][col] 
-        || board[row][col]=='X'){
+    public void dfs(int row, int col, int n, int m, char[][] board){
+        if(row<0 || col<0 || row>=n || col>=m || board[row][col]=='X' || board[row][col]=='!'){
             return;
         }
 
-        visited[row][col] = true;
-        dfs(row-1, col, n, m, board, visited);
-        dfs(row+1, col, n, m, board, visited);
-        dfs(row, col-1, n, m, board, visited);
-        dfs(row, col+1, n, m, board, visited);
+        board[row][col] = '!';
+        dfs(row-1, col, n, m, board);
+        dfs(row+1, col, n, m, board);
+        dfs(row, col-1, n, m, board);
+        dfs(row, col+1, n, m, board);
     }
 }
