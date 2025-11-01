f451c2f6ecc66749efa9cd9567c2a33a499c7d07
Vishnu <153791406+vardhan998997@users.noreply.github.com>
2025-11-01 17:52:08 +0530

    Time: 9 ms (9.89%) | Memory: 44.9 MB (6.76%) - LeetSync

--- 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.java
+++ 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.java
@@ -5,7 +5,6 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
 
-        // Step 1: Flip all possible negative numbers
         for (int i = 0; i < n && k > 0; i++) {
             if (nums[i] < 0) {
                 nums[i] = -nums[i];
@@ -15,7 +14,6 @@ class Solution {
             }
         }
 
-        // Step 2: If still k left, flip the smallest absolute value if k is odd
         int min = Arrays.stream(nums).min().getAsInt();
         if (k % 2 == 1) {
             int minAbs = Math.abs(min);
