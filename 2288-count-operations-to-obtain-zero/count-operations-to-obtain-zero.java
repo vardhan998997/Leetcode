716e1a645edacb5a9d0365cb4ed511f297620df2
Vishnu <153791406+vardhan998997@users.noreply.github.com>
2025-11-09 12:43:13 +0530

    Time: 1 ms (90.35%) | Memory: 42.3 MB (14.07%) - LeetSync

---
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countOperations(int num1, int num2) {
+        if(num1 == num2 && num1!=0) return 1;
+
+        int ans = 0;
+        while(num1!=0 && num2!=0){
+            if(num1<num2){
+                num2 -= num1;
+            }else{
+                num1 -= num2;
+            }
+
+            ans++;
+
+            if(num1==1 || num2==1){
+                return ans + Math.max(num1, num2);
+            }
+        }
+        return ans;
+    }
+}
